133ae2f70219d0fd244a783947013e8966c6be92
DV7798 <48938932+DV7798@users.noreply.github.com>
2020-02-04 21:14:11 +0530

    Update p12.py

--- p12.py
+++ p12.py
@@ -1,6 +1,10 @@
-n=int(input('Enter the number'))
-f=1
-while n>=1:
-    f=f*n
-    n-=1
-print('Factorial is ',f)
+def fact(n):
+    f=1
+    while n>=1:
+        f=f*n
+        n-=1
+    return f
+
+n=int(input('Enter the number '))
+f=fact(n)
+print('Factorial is',f)
